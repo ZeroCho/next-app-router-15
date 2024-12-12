0200208c2eeb4f5c45cd5b8f1b30e21f0c421c60
zerocho <zerohch0@gmail.com>
2024-12-13 03:35:44 +0900

    add: 회원가입 실제로 하기

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -21,12 +21,17 @@ function showMessage(message: string | null | undefined) {
   if (message === 'user_exists') {
     return '이미 사용 중인 아이디입니다.';
   }
-  return '';
+  if (message === 'nickname must be a string') {
+    return '닉네임이 필요합니다.';
+  }
+  return message;
 }
 
 export default function SignupModal() {
   const [state, formAction] = useFormState(onSubmit, { message: null });
   const { pending } = useFormStatus();
+
+  console.log('state', state);
 
   return (
     <>
@@ -41,24 +46,26 @@ export default function SignupModal() {
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="id">아이디</label>
                 <input id="id" name="id" className={style.input} type="text" placeholder=""
-                       required
+                       required defaultValue={state.id as string}
                 />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="name">닉네임</label>
                 <input id="name" name="name" className={style.input} type="text" placeholder=""
-                       required
+                       required defaultValue={state.nickname as string}
                 />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="password">비밀번호</label>
                 <input id="password" name="password" className={style.input} type="password" placeholder=""
-                       required
+                       required defaultValue={state.password as string}
                 />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="image">프로필</label>
-                <input id="image" name="image" required className={style.input} type="file" accept="image/*"
+                <input id="image" name="image"
+                       required className={style.input} type="file" accept="image/*"
+                       defaultValue={state.image as string}
                 />
               </div>
             </div>
