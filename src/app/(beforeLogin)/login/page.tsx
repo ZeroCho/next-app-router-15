70dc3d860c19dda8585b9700308a41bd8ceaee81
zerocho <zerohch0@gmail.com>
2024-12-19 15:39:33 +0900

    add: 배포 직전 build하기

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,19 +1,21 @@
-'use client';
-
-import {useRouter} from "next/navigation";
 import Main from "@/app/(beforeLogin)/_component/Main";
-import { useSession } from 'next-auth/react';
+import {auth} from "@/auth";
+import {redirect} from "next/navigation";
+import RedirectToLogin from "@/app/(beforeLogin)/login/_component/RedirectToLogin";
 
-export default function Login() {
-  const router = useRouter();
+export default async function Login() {
 
-  const { data: session }  = useSession();
+  const session = await auth();
 
   if (session?.user) {
-    router.replace('/home');
+    redirect('/home');
     return null;
   }
 
-  router.replace('/i/flow/login')
-  return <Main />;
+  return (
+    <>
+      <RedirectToLogin/>
+      <Main/>
+    </>
+  );
 }
