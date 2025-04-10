cf5ccd6c36cbea23283373bf551068e1e6704e32
zerocho <zerohch0@gmail.com>
2025-04-10 15:25:29 +0900

    fix: for build

--- src/app/(afterLogin)/messages/_component/Room.tsx
+++ src/app/(afterLogin)/messages/_component/Room.tsx
@@ -2,50 +2,47 @@
 
 import style from "@/app/(afterLogin)/messages/message.module.css";
 import dayjs from "dayjs";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 import relativeTime from "dayjs/plugin/relativeTime";
-import 'dayjs/locale/ko';
-import { Room } from '@/model/Room';
-import {useSession} from "next-auth/react";
+import "dayjs/locale/ko";
+import { Room as RoomModel } from "@/model/Room";
+import { useSession } from "next-auth/react";
 
-dayjs.locale('ko');
-dayjs.extend(relativeTime)
+dayjs.locale("ko");
+dayjs.extend(relativeTime);
 
 type Props = {
-  room: Room,
+  room: RoomModel;
 };
 
 export default function Room({ room }: Props) {
   const { data: session } = useSession();
   const router = useRouter();
 
-  const onClick =() => {
+  const onClick = () => {
     router.push(`/messages/${room.room}`);
   };
 
-  const user = room.Receiver.id === session?.user?.email ? room.Sender : room.Receiver;
+  const user =
+    room.Receiver.id === session?.user?.email ? room.Sender : room.Receiver;
 
   return (
     <div className={style.room} onClickCapture={onClick}>
       <div className={style.roomUserImage}>
-        <img src={user.image} alt=""/>
+        <img src={user.image} alt="" />
       </div>
       <div className={style.roomChatInfo}>
         <div className={style.roomUserInfo}>
           <b>{user.nickname}</b>
           &nbsp;
           <span>@{user.id}</span>
-          &nbsp;
-          ·
-          &nbsp;
+          &nbsp; · &nbsp;
           <span className={style.postDate}>
             {dayjs(room.createdAt).fromNow(true)}
           </span>
         </div>
-        <div className={style.roomLastChat}>
-          {room.content}
-        </div>
+        <div className={style.roomLastChat}>{room.content}</div>
       </div>
     </div>
-  )
+  );
 }
